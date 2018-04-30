0a41712186ad69d237d82f35f7a1af74435c9172
Cloud TV India <indiacloudtv@gmail.com>
2018-04-30 23:04:25 +0530

    Changed the code better readability

--- src/main/scala/com/cloudtv/spark/samples/demo.scala
+++ src/main/scala/com/cloudtv/spark/samples/demo.scala
@@ -1,5 +1,6 @@
 package com.cloudtv.spark.samples
 
+import org.apache.spark.sql.{SQLContext, SparkSession}
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.SparkContext._
 
@@ -7,7 +8,13 @@ object demo {
   def main(args: Array[String]) {
 
     println("Spark WordCount Sample Started ...")
-    val sc = new SparkContext("spark://127.0.0.1:7077", "Word Count Demo")
+    val spark = SparkSession.builder()
+      .master(master = "local[*]")
+      .appName("Word Count Demo")
+      .getOrCreate()
+
+    val sc = spark.sparkContext
+    //val sc = new SparkContext("spark://127.0.0.1:7077", "Word Count Demo")
 
     // creating an linesRDD by loading text file (hadoop.txt) from HDFS through Spark context
 
@@ -15,9 +22,11 @@ object demo {
 
     val wordCounts = linesRDD.flatMap(line => line.split(" ")).map(word => (word, 1)).reduceByKey(_+_)
 
-    wordCounts.count()
+    println("Total words: " + wordCounts.count())
 
-    wordCounts.collect()
+    println("All the words and occurrence count for each word: ")
+    val wordCountsScala = wordCounts.collect()
+    wordCountsScala.foreach(item => println("Word: " + item._1 + " --> " + "Occurrence count: " + item._2))
 
     println("Spark WordCount Sample Completed.")
 
